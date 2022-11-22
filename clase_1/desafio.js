0fbc208966f67dbb2da6aed538d92e7f99b33c66
Emiliano Perez <117744347+emiliano997@users.noreply.github.com>
2022-11-21 22:32:38 -0300

    Clase 1 Agregada

---
+++ clase_1/desafio.js
@@ -0,0 +1,10 @@
+let nombre = 'Emiliano'
+let edad = 25
+let precio = 1.50
+let series = ['Breaking bad']
+let peliculas = ['Avengers']
+
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+console.log(series)
